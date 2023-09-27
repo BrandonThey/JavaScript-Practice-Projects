ff304a05ef36ea36d2de8bac34a93aae71424ca8
Brandon They <theyb5220@gmail.com>
2023-09-27 17:28:48 -0500

    Added the ability to add items to the list

--- Project 1 - Shopping List/script.js
+++ Project 1 - Shopping List/script.js
@@ -9,15 +9,46 @@ const addItem = (e) =>{
     //prevent the default action of sumbitting
     e.preventDefault();
 
+    //creating a newItem variable to hold the new item from the form
+    const newItem = itemInput.value;
+
     //adding basic validation
-    if(itemInput.value === ""){
+    if(newItem === ""){
         alert("Please add an item");
         return;
     }
 
-    console.log("Success");
+    //creating a variable to hold the item
+    const listItem = document.createElement("li");
+    //appending item data to the list 
+    listItem.appendChild(document.createTextNode(newItem))
+
+    //append tab to the item
+    const itemTab = createTab("remove-item btn-link text-red");
+    listItem.appendChild(itemTab);
+
+    //appending the list item to the items list
+    itemList.appendChild(listItem)
+
+    //resetting the item input
+    itemInput.value="";
 }
 
-console.log("IN SCRIPT.Js")
+//function that creates a item tab given the classes to be used with it
+const createTab = (classes) => {
+    const item = document.createElement("button");
+    item.className = classes;
+    const icon = createIcon("fa-solid fa-xmark");
+    item.appendChild(icon);
+    return item;
+}
+
+//function that creates an icon for the item given the classes
+const createIcon = (classes) => {
+    const icon = document.createElement("i");
+    icon.className = classes;
+    return icon;
+}
+
 //Adding event listener 
 itemForm.addEventListener("submit", addItem);
