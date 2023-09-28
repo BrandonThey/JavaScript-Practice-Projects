40947ef34ecb699aac5525c1737c928665e37097
Brandon They <theyb5220@gmail.com>
2023-09-28 14:30:50 -0500

    added a simple page router and made it so that a nav link is highlighted whenever a user is on that nav linked page

--- Project 2 - Flixx Movie Application/js/script.js
+++ Project 2 - Flixx Movie Application/js/script.js
@@ -3,31 +3,39 @@ const global = {
     currentPage: window.location.pathname
 }
 
-//initializing function
+//highlight active link of the page we're on
+const highlightActiveLink = () => {
+    //grab all the nav links and highlight the links
+    //that match our current page
+    const links = document.querySelectorAll(".nav-link")
+    links.forEach((link) => {
+        if(link.getAttribute("href") === global.currentPage){
+            link.classList.add("active");
+        }
+    })
+}
+
+//initializing function that runs every time a page is loaded
 const init = () => {
     //switch statement that determines what action to take
     //based on what page the user is on
     //serves as a simple router
     switch(global.currentPage){
+        // the / and the index.html pages are the same
         case "/":
-            break;
-
         case "/index.html":
-            console.log("home");
             break;
         case "/shows.html":
-            console.log("shows");
             break;
         case "/movie-details.html":
-            console.log("movie details");
             break;
         case "/tv-details.html":
-            console.log("tv details");
             break;
         case "/search.html":
-            console.log("search");
             break;
     }
+
+    highlightActiveLink();
 }
 
 init();
