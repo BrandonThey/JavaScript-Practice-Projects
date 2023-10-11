60cce948fb57ccbb6c15f6f926ee9902caf275e0
Brandon They <theyb5220@gmail.com>
2023-10-11 18:23:17 -0500

    Defined a basic server that listens on port 5000 and returns a hello world statement when a get request is sent to the index page

--- Project 4 - RandomIdeas App Backend/server.js
+++ Project 4 - RandomIdeas App Backend/server.js
@@ -1,4 +1,16 @@
+//bringing in express and defining our port
 const express = require("express");
 const port = 5000;
 
+//setting our app to be express
 const app = express();
+
+//defining out get route for the index page
+app.get("/", (request, response) => {
+    //currently when some sends a get request for the index page
+    //we just respond with hello world
+    response.send("Hello World");
+})
+
+//listening for any requests on our port
+app.listen(port, () => console.log("Listening on port " + port));
