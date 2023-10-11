af716e1677358fe360204e383763138e016f7cc6
Brandon They <theyb5220@gmail.com>
2023-10-11 18:38:02 -0500

    Defined a route to get the api's ideas and some place holder data to showcase the get

--- Project 4 - RandomIdeas App Backend/server.js
+++ Project 4 - RandomIdeas App Backend/server.js
@@ -5,11 +5,39 @@ const port = 5000;
 //setting our app to be express
 const app = express();
 
-//defining out get route for the index page
+//defining a get route for the index
 app.get("/", (request, response) => {
-    //currently when some sends a get request for the index page
+    //currently when some sends a get request for the index
     //we just respond with hello world
-    response.send("Hello World");
+    //for example if you start the server and send a get request
+    //through postman or the url (http://localhost:5000) it will return a hello world
+    response.json({message: "Welcome to the Random Ideas API"});
+})
+
+
+//some placeholder idea data (array of idea objects) 
+//to be sent back as a response to
+//the api/ideas route
+const ideas = [
+    {
+        id: 1,
+        text: "Some text here",
+        tag: "Tech",
+        username: "BThey",
+        date: "2023-01-01"
+    },
+    {
+        id: 2,
+        text: "Some other text here",
+        tag: "Marketing",
+        username: "JasonB",
+        date: "2023-03-04"
+    }
+]
+//defining a get route for the api's saved ideas 
+app.get("/api/ideas", (request, response) => {
+    //responding with a successful request and the ideas data
+    response.json({success: true, data: ideas});
 })
 
 //listening for any requests on our port
