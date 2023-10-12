65460252b4f0be9a17e42668ef5cf984febe9d6a
Brandon They <theyb5220@gmail.com>
2023-10-12 13:18:12 -0500

    Refactored server.js by moving all the ideas routes to its own file and importing that file

--- Project 4 - RandomIdeas App Backend/server.js
+++ Project 4 - RandomIdeas App Backend/server.js
@@ -14,46 +14,10 @@ app.get("/", (request, response) => {
     response.json({message: "Welcome to the Random Ideas API"});
 })
 
+//importing the ideas route and assigning it an endpoint and using it 
+const ideasRouter = require("./routes/ideas");
+app.use("/api/ideas", ideasRouter)
 
-//some placeholder idea data (array of idea objects) 
-//to be sent back as a response to
-//the api/ideas route
-const ideas = [
-    {
-        id: 1,
-        text: "Some text here",
-        tag: "Tech",
-        username: "BThey",
-        date: "2023-01-01"
-    },
-    {
-        id: 2,
-        text: "Some other text here",
-        tag: "Marketing",
-        username: "JasonB",
-        date: "2023-03-04"
-    }
-]
-//defining a get route for the api's saved ideas 
-app.get("/api/ideas", (request, response) => {
-    //responding with a successful request and the ideas data
-    response.json({success: true, data: ideas});
-})
 
-//defining a get route for specific ideas based on their ids
-//uses a query param to get the id from the get request
-app.get("/api/ideas/:id", (request, response) => {
-    //finding the idea
-    const idea = ideas.find((idea) => idea.id === +request.params.id)
-    
-    //testing to see if we found the idea
-    if(idea){
-        //responding with a successful request and the specific idea
-        response.json({success: true, data: idea});
-    } else{
-        //sending a not found status and an error response 
-        response.status(404).json({success: false, error: "Idea not found"})
-    }
-})
 //listening for any requests on our port
 app.listen(port, () => console.log("Listening on port " + port));
